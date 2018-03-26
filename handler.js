7e810fc7535a9506a2022afdc744043f9d59eaed
Cliff Chao-kuan Lu <clifflu@gmail.com>
2018-03-26 11:47:52 +0800

    feat: basic

---
+++ handler.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+function hdlr_check(evt, ctx, cb) {
+  function request_callback(err, response, body) {
+    if (!err && -1 === body.indexOf(process.env.expect)) {
+      err = new Error('Expect not found in body')
+    }
+    
+    cb(err)
+  }
+
+  request.get(process.env['uri'], request_callback)
+}
+
+module.exports = {
+  hdlr_check,
+}
