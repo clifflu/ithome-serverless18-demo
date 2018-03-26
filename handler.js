5f3ef5ffdbebd02c2a268575b7b20a8f390687ab
Cliff Chao-kuan Lu <clifflu@gmail.com>
2018-03-26 15:03:32 +0800

    fix: errors with child_process.exec and response parsing

--- handler.js
+++ handler.js
@@ -2,9 +2,18 @@ const child_process = require('child_process')
 
 function hdlr_check(evt, ctx, cb) {
   function exec_cb(err, stdout, stderr) {
-    let output = JSON.parse(stdout)
-    console.log(output)
-    cb(null, output.time.total)
+    if (err) {
+      return cb(err)
+    }
+
+    console.log({stdout, stderr})
+
+    try {
+      let output = JSON.parse(stdout)
+      cb(null, output.time.total)  
+    } catch(E) {
+      cb(new Error('Invalid output, expecting JSON'))
+    }
   }
 
   child_process.exec(
